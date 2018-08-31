268d436a42f85694ed704b7ea222c5999059488e
Gabriel Felix <crowleyfelix@gmail.com>
2018-08-31 09:23:04 -0300

    Add  test fixtures

---
+++ tests/unit/utils/fixtures.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+parse = datetime.strptime
+
+dtfmt = "%Y-%m-%dT%H:%M:%SZ"
+tfmt = "%H:%M"
+
+START_RANGE = parse("2018-02-28T21:57:13Z", dtfmt)
+END_RANGE = parse("2018-03-01T22:10:56Z", dtfmt)
+
+STANDARD_CYCLE = {
+    "start": parse("06:00", tfmt).time(),
+    "end": parse("22:00", tfmt).time(),
+    "ocurrencies": [
+        (parse("2018-02-28T21:57:13Z", dtfmt),
+         parse("2018-02-28T22:00:00Z", dtfmt)),
+        (parse("2018-03-01T06:00:00Z", dtfmt),
+         parse("2018-03-01T22:22:00Z", dtfmt))
+    ]
+}
+
+REDUCED_CYCLE = {
+    "start": parse("22:00", tfmt).time(),
+    "end": parse("06:00", tfmt).time(),
+    "ocurrencies": [
+        (parse("2018-02-28T22:00:00Z", dtfmt),
+         parse("2018-03-01T06:00:00Z", dtfmt)),
+        (parse("2018-03-01T22:22:00Z", dtfmt),
+         parse("2018-03-01T22:10:56Z", dtfmt))
+    ]
+}
--- tests/unit/utils/test_date_time.py
+++ tests/unit/utils/test_date_time.py
@@ -1,34 +1,31 @@
 from datetime import datetime
 from unittest import TestCase
 from app.utils.date_time import get_cycle_ocurrencies
-
-fmt = "%Y-%m-%dT%H:%M:%SZ"
-start_range = datetime.strptime("2018-02-28T21:57:13Z", fmt)
-end_range = datetime.strptime("2018-03-01T22:10:56Z", fmt)
-
-start_cycle = datetime.strptime("06:00", "%H:%M").time()
-end_cycle = datetime.strptime("22:00", "%H:%M").time()
-
-start_cycle = datetime.strptime("22:00", "%H:%M").time()
-end_cycle = datetime.strptime("06:00", "%H:%M").time()
-
-# Standard
-standard_ocurrencies = [
-    ("2018-02-28T21:57:13Z", "2018-02-28T22:00:00Z"),
-    ("2018-03-01T06:00:00Z", "2018-03-01T22:22:00Z")
-]
-
-# Reduced
-reduced_ocurrencies = [
-    ("2018-02-28T22:00:00Z", "2018-03-01T06:00:00Z"),
-    ("2018-03-01T22:22:00Z", "2018-03-01T22:10:56Z")
-]
+from . import fixtures
 
 
 class TestDateTime(TestCase):
     def test_get_cycle_ocurrencies(self):
-        ocurrencies = get_cycle_ocurrencies(
-            start_range, end_range, start_cycle, end_cycle)
+        with self.subTest("with standard cycle"):
+            start_range = fixtures.START_RANGE
+            end_range = fixtures.END_RANGE
+            start_cycle = fixtures.STANDARD_CYCLE["start"]
+            end_cycle = fixtures.STANDARD_CYCLE["end"]
+            expected = fixtures.STANDARD_CYCLE["ocurrencies"]
 
-        import pdb
-        pdb.set_trace()
+            actual = get_cycle_ocurrencies(
+                start_range, end_range, start_cycle, end_cycle)
+
+            self.assertEqual(actual, expected)
+
+        with self.subTest("with reduced cycle"):
+            start_range = fixtures.START_RANGE
+            end_range = fixtures.END_RANGE
+            start_cycle = fixtures.REDUCED_CYCLE["start"]
+            end_cycle = fixtures.REDUCED_CYCLE["end"]
+            expected = fixtures.REDUCED_CYCLE["ocurrencies"]
+
+            actual = get_cycle_ocurrencies(
+                start_range, end_range, start_cycle, end_cycle)
+
+            self.assertEqual(actual, expected)
