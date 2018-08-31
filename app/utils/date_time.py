dbc0ed5ebf6d7e8a960f6ffe48c81b010254c047
Gabriel Felix <crowleyfelix@gmail.com>
2018-08-31 16:50:56 -0300

    Add pandas dependencie

--- app/utils/date_time.py
+++ app/utils/date_time.py
@@ -1,84 +1,29 @@
 """Module with datetime processors."""
-from datetime import timedelta
+import pandas
+
+PANDAS_MIN_FREQUENCY = "min"
 
 
-class DateTimeCycle(object):
-    """Class to handle with datetime cycles."""
+def get_cycle_ocurrencies(cls, start_range, end_range,
+                          start_cycle, end_cycle):
+    """Get cycle ocurrencies from date range ."""
+    ocurrencies = []
 
-    @classmethod
-    def get_cycle_ocurrencies(cls, start_range, end_range,
-                              start_cycle, end_cycle):
-        """Get cycle ocurrencies from date range ."""
-        ocurrencies = []
+    range_datetime = pandas.date_range(start_range,
+                                       end_range,
+                                       freq=PANDAS_MIN_FREQUENCY)
 
-        started_cycle, ended_cycle = cls.find_current_cycle(
-            start_range, start_cycle, end_cycle)
+    serie = range_datetime.to_series()
+    filtered = serie.between_time(start_cycle, end_cycle)
 
-        while start_range < end_range:
+    started_cycle = None
+    for item in filtered:
 
-            # If start range is between cycle, append ocurrencies
-            if is_in_range(start_range, started_cycle, ended_cycle):
-                if ended_cycle < end_range:
-                    ocurrencies.append((start_range, ended_cycle))
-                else:
-                    ocurrencies.append((start_range, end_range))
+        if not started_cycle:
+            started_cycle = item
 
-            start_range = to_next_time(start_range, start_cycle)
+        if item.time() == end_cycle:
+            ocurrencies.append((started_cycle, item))
+            started_cycle = None
 
-            # If start range is greater then start cycle, go to next cycle
-            if start_range > started_cycle:
-                started_cycle, ended_cycle = cls.get_next_cycle(
-                    started_cycle, ended_cycle)
-
-        return ocurrencies
-
-    @staticmethod
-    def find_current_cycle(reference_datetime, start_cycle, end_cycle):
-        """Find current cycle from date reference."""
-        if is_in_range(reference_datetime.time(), start_cycle, end_cycle):
-            started_cycle = to_previous_time(reference_datetime, start_cycle)
-        else:
-            started_cycle = to_next_time(reference_datetime, start_cycle)
-
-        ended_cycle = to_next_time(started_cycle, end_cycle)
-
-        return (started_cycle, ended_cycle)
-
-    @staticmethod
-    def get_next_cycle(started_cycle, ended_cycle):
-        """Get to next cycle."""
-        started_cycle = to_next_time(started_cycle, started_cycle.time())
-        ended_cycle = to_next_time(ended_cycle, ended_cycle.time())
-
-        return (started_cycle, ended_cycle)
-
-
-def is_in_range(reference, start_range, end_range):
-    """Is reference in range."""
-    return start_range <= reference <= end_range
-
-
-def to_previous_time(datetime, time):
-    """Get to previous time."""
-    days_to_add = 0
-
-    if datetime.time() <= time:
-        days_to_add = 1
-
-    datetime -= timedelta(days=days_to_add)
-    return datetime.replace(hour=time.hour,
-                            minute=time.minute,
-                            second=time.second)
-
-
-def to_next_time(datetime, time):
-    """Get to next time."""
-    days_to_add = 0
-
-    if datetime.time() >= time:
-        days_to_add = 1
-
-    datetime += timedelta(days=days_to_add)
-    return datetime.replace(hour=time.hour,
-                            minute=time.minute,
-                            second=time.second)
+    return ocurrencies
