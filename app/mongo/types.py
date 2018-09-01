b4a6646c56435888f8ec46eaa38322f2b58e86fc
Gabriel Felix <crowleyfelix@gmail.com>
2018-09-01 12:28:26 -0300

    Add docs

--- app/mongo/types.py
+++ app/mongo/types.py
@@ -1,11 +1,19 @@
+"""Module for mongo types."""
+
+
 class List(object):
+    """List cursor."""
+
     def __init__(self, cursor):
+        """Initialize attributes."""
         self._cursor = cursor
 
     def __iter__(self):
+        """Get iterable."""
         return self
 
     def __next__(self):
+        """Get next value."""
         try:
             document = self._cursor.next()
             document.pop("_id")
