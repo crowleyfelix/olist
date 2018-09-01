3ea501413ccb2ea8daba1b5273e09d4a8d8c6de1
Gabriel Felix <crowleyfelix@gmail.com>
2018-09-01 16:48:52 -0300

    Add datetime parsers

--- app/utils/datetime.py
+++ app/utils/datetime.py
@@ -1,9 +1,11 @@
 """Module with datetime processors."""
 import pandas
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
-DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
-TIME_FORMAT = "%H:%M"
+DATETIME_FMT = "%Y-%m-%dT%H:%M:%SZ"
+TIME_FMT = "%H:%M"
+YEAR_MONTH_FMT = "%Y-%m"
 
 
 def date_range(start, end):
@@ -45,9 +47,29 @@ def get_cycle_ocurrencies(start_range, end_range,
 
 def to_time(raw):
     """Convert to time."""
-    return datetime.strptime(raw, TIME_FORMAT).time()
+    return datetime.strptime(raw, TIME_FMT).time()
 
 
-def to_datetime(raw):
+def to_datetime(raw, fmt=DATETIME_FMT):
     """Convert to datetime."""
-    return datetime.strptime(raw, DATETIME_FORMAT)
+    return datetime.strptime(raw, fmt)
+
+
+def begin_end_month(period):
+    """Get begin day and end day month from period."""
+    begin = to_datetime(period, YEAR_MONTH_FMT)
+    end = begin + relativedelta(months=1, days=-1)
+    return (begin, end)
+
+
+def begin_end_previous_month():
+    """Get begin day and end day month from previous month."""
+    reference_date = datetime.today() - relativedelta(month=1)
+
+    period = f"{reference_date.year}{reference_date.month}"
+    return begin_end_month(period)
+
+
+def from_timestamp(timestamp):
+    """Convert timestamp to datetime."""
+    return datetime.fromtimestamp(timestamp)
